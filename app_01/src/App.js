fbbaf883909c2e8010351790aad2d4b89d158fe0
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2023-05-01 11:53:22 +0300

    Summary-test-react

---
+++ app_01/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+      <p className="text">Hello react text</p>
+      <select>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+      </select>
+      <hr />
+      <img src="./images/react.png" alt="react" title="react" width="200" />
+     
+    </div>
+  );
+}
+
+export default App;
